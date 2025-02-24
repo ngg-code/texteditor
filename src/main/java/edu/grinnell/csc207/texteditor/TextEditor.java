3463aac277dd3719520012f6ec21055b67888e8a
ngg-code <157342451+ngg-code@users.noreply.github.com>
2025-02-24 05:38:30 +0000

    Format

--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -1,7 +1,9 @@
 package edu.grinnell.csc207.texteditor;
+
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.screen.Screen;
 import java.io.IOException;
+
 /**
  * The driver for the TextEditor Application.
  */
@@ -19,7 +21,7 @@ public class TextEditor {
         }
 
         GapBuffer gapBuffer = new GapBuffer();
-        
+
         Screen screen = new DefaultTerminalFactory().createScreen();
         screen.startScreen();
         String path = args[0];
@@ -28,40 +30,40 @@ public class TextEditor {
 
         if (Files.exists(filePath) && Files.isRegularFile(filePath)) {
             String gapBufferStr = Files.readString(filePath);
-            for (int i = 0; i < gapBufferStr.length(); i++){
+            for (int i = 0; i < gapBufferStr.length(); i++) {
                 gapBuffer.insert(gapBufferStr.charAt(i));
             }
         }
     }
 
     boolean isRunning = true;
-    
-        while (isRunning) {
-            drawBuffer(gapBuffer, screen);
-            KeyStroke stroke = screen.readInput();
 
-            if (stroke.getKeyType() == KeyType.Character) {
-                gapBuffer.insert(stroke.getCharacter());
-            } 
-            
-            if (stroke.getKeyType() == KeyType.Backspace) {
-                gapBuffer.delete();
-            } 
-            
-            if (stroke.getKeyType() == KeyType.ArrowLeft) {
-                gapBuffer.moveLeft();
-            }
-            
-            if (stroke.getKeyType() == KeyType.ArrowRight) {
-                gapBuffer.moveRight();
-            } 
-            
-            if (stroke.getKeyType() == KeyType.Escape){
-                screen.stopScreen();
-                isRunning = false;
-            }
+    while(isRunning)
+    {
+        drawBuffer(gapBuffer, screen);
+        KeyStroke stroke = screen.readInput();
+
+        if (stroke.getKeyType() == KeyType.Character) {
+            gapBuffer.insert(stroke.getCharacter());
         }
-        
-        Files.writeString(filePath, gapBuffer.toString());
+
+        if (stroke.getKeyType() == KeyType.Backspace) {
+            gapBuffer.delete();
+        }
+
+        if (stroke.getKeyType() == KeyType.ArrowLeft) {
+            gapBuffer.moveLeft();
+        }
+
+        if (stroke.getKeyType() == KeyType.ArrowRight) {
+            gapBuffer.moveRight();
+        }
+
+        if (stroke.getKeyType() == KeyType.Escape) {
+            screen.stopScreen();
+            isRunning = false;
+        }
     }
-}
+
+    Files.writeString(filePath,gapBuffer.toString());
+}}
