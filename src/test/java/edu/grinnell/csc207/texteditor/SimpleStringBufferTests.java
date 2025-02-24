52e3918a64f1e04e522ea54eac8070236eafddef
ngg-code <157342451+ngg-code@users.noreply.github.com>
2025-02-24 01:18:53 +0000

    Add libraries for test and assert

--- src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
@@ -1,4 +1,6 @@
 package edu.grinnell.csc207.texteditor;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleStringBufferTests {
     /** TODO: fill me in with unit and property tests! */
