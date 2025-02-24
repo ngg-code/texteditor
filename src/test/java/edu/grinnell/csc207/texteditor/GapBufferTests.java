6f3bfff1faa0b95fe88de46abd78feae3fa983af
ngg-code <157342451+ngg-code@users.noreply.github.com>
2025-02-24 05:11:11 +0000

    Test for insert, delete, toString, getSize, getCursorPosition

--- src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/GapBufferTests.java
@@ -1,5 +1,22 @@
 package edu.grinnell.csc207.texteditor;
 
 public class GapBufferTests {
-    /** TODO: fill me in with unit and property tests! */
+    private GapBuffer buffer;
+
+    @Test
+    void testInsertCharacters() {
+        buffer.insert('H');
+        buffer.insert('e');
+        buffer.insert('l');
+        buffer.insert('l');
+        buffer.insert('o');
+
+        assertEquals("Hello", buffer.toString());
+        assertEquals(5, buffer.getSize());
+        assertEquals(5, buffer.getCursorPosition());
+        buffer.delete();
+        assertEquals("Hell", buffer.toString());
+        assertEquals(0, buffer.getSize());
+        assertEquals(0, buffer.getCursorPosition());
+    }
 }
