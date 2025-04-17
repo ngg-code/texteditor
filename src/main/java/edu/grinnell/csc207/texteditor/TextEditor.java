60ddb5edc6435da2b38ca1117ccfd5ec4e762d15
ngg-code <157342451+ngg-code@users.noreply.github.com>
2025-04-17 02:34:26 +0000

    format change

--- src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
+++ src/main/java/edu/grinnell/csc207/texteditor/TextEditor.java
@@ -82,9 +82,8 @@ public class TextEditor {
 
         for (int i = 0; i < content.length(); i++) {
             screen.setCharacter(
-                new TerminalPosition(i, 0),
-                new TextCharacter(content.charAt(i), TextColor.ANSI.DEFAULT, TextColor.ANSI.DEFAULT)
-            );
+                    new TerminalPosition(i, 0),
+                    new TextCharacter(content.charAt(i), TextColor.ANSI.DEFAULT, TextColor.ANSI.DEFAULT));
         }
 
         screen.setCursorPosition(new TerminalPosition(gapBuffer.getCursorPosition(), 0));
