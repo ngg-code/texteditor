1c2a3f24b20688752e1ba32cd32b0ce62e650bf1
ngg-code <157342451+ngg-code@users.noreply.github.com>
2025-04-17 03:01:30 +0000

    format error fixed

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -4,7 +4,7 @@ package edu.grinnell.csc207.texteditor;
  * A gap buffer-based implementation of a text buffer.
  */
 public class GapBuffer {
-     /** The character array storing the buffer's content and gap */
+    /** The character array storing the buffer's content and gap */
     public char[] str;
     /** The index of the cursor in the buffer */
     public int indexI;
@@ -26,11 +26,13 @@ public class GapBuffer {
         this.size = 0;
     }
 
-    /** 
+    /**
      * Inserts a character at the current cursor position.
-     * If the buffer is full, it expands the buffer to accommodate the new character.
+     * If the buffer is full, it expands the buffer to accommodate the new
+     * character.
+     * 
      * @param ch the character to insert
-    */
+     */
     public void insert(char ch) {
         if (this.indexI == this.indexF) {
             expandBuffer();
@@ -53,6 +55,7 @@ public class GapBuffer {
 
     /**
      * Returns the current cursor position in the buffer.
+     * 
      * @return the index of the cursor
      */
     public int getCursorPosition() {
@@ -89,6 +92,7 @@ public class GapBuffer {
 
     /**
      * Returns the current size of the buffer, excluding the gap.
+     * 
      * @return the size of the buffer
      */
     public int getSize() {
@@ -97,6 +101,7 @@ public class GapBuffer {
 
     /**
      * Returns the character at the specified index in the buffer.
+     * 
      * @param i the index of the character to retrieve
      * @return the character at the specified index
      */
@@ -107,6 +112,7 @@ public class GapBuffer {
     /**
      * Returns a string representation of the buffer, excluding the gap.
      * The characters before the gap are followed by the characters after the gap.
+     * 
      * @return a string representation of the buffer
      */
     public String toString() {
@@ -139,10 +145,8 @@ public class GapBuffer {
         int afterTextStart = newSize - (this.str.length - this.indexF);
         System.arraycopy(this.str, 0, newBuffer, 0, this.indexI);
         System.arraycopy(
-    this.str, this.indexF, newBuffer,
-    afterTextStart, this.str.length - this.indexF
-);
-
+                this.str, this.indexF, newBuffer,
+                afterTextStart, this.str.length - this.indexF);
 
         this.indexF = afterTextStart;
         this.str = newBuffer;
@@ -151,6 +155,7 @@ public class GapBuffer {
     /**
      * Expands the result array to double its size.
      * This method is used when the result array is full and needs to be expanded.
+     * 
      * @param result the character array to expand
      */
     private void expand(char[] result) {
